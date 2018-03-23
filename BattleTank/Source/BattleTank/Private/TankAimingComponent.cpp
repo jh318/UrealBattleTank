0e05e266cefab3f4fd44ce88355592cc3c4784d7
John <jhawley318@gmail.com>
2018-03-23 09:13:18 -0500

    created mortar with existing tank components

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -62,7 +62,7 @@ bool UTankAimingComponent::IsBarrelMoving()
 {
 	if (!ensure(Barrel)) { return false; }
 	auto BarrelForward = Barrel->GetForwardVector();
-	return !BarrelForward.Equals(AimDirection, 0.01);
+	return !BarrelForward.Equals(AimDirection, 0.1);
 }
 
 void UTankAimingComponent::AimAt(FVector HitLocation)
